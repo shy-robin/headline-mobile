f7bd5fe274a9debc28bc14e62e3456c29afcf192
Shallowbeing <load_more@163.com>
2021-08-13 18:46:37 +0800

    feat: 引入图标字体，和使用link标签引入不同，这种方式可以离线加载图标

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@
       <van-cell title="单元格" value="内容"></van-cell>
       <van-cell title="单元格" value="内容"></van-cell>
     </van-cell-group>
+    <div>
+      <i class="iconfont icon-shoucang_quxiaoshoucang"></i>
+      <i class="iconfont icon-sucai"></i>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import './style/index.scss';
 </style>
