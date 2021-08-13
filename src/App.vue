7f278a3b03aea793c6e107326013513d45ca0697
Shallowbeing <load_more@163.com>
2021-08-13 11:18:32 +0800

    package: 注册并使用VantUI组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     Hello world
+    <van-cell-group>
+      <van-cell title="单元格" value="内容"></van-cell>
+      <van-cell title="单元格" value="内容"></van-cell>
+    </van-cell-group>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 注册 Vant 组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
