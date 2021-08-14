f79984ddc6e35f6303f4d2a15304497cfa880d8f
Shallowbeing <load_more@163.com>
2021-08-14 21:03:09 +0800

    style: 首页-顶栏 - 样式布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home-container">
-    首页
+    <van-nav-bar class="nav">
+      <van-button
+        class="search-button"
+        slot="title"
+        icon-prefix="iconfont"
+        icon="sousuo"
+        color="#5babfb"
+        round
+      >搜索</van-button>
+    </van-nav-bar>
   </div>
 </template>
 
@@ -11,5 +20,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.home-container {
+  .nav {
+    height: 48px;
+    .search-button {
+      width: 278px;
+      height: 32px;
+      font-size: 14px;
+    }
+    ::v-deep .van-nav-bar__title {
+      max-width: none;
+    }
+  }
+}
 </style>
