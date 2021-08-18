701a9581e519d699be3d44ffaaa7a70a111aba29
Shallowbeing <load_more@163.com>
2021-08-18 22:21:31 +0800

    feat: 利用axios响应拦截器处理请求失败的问题以及刷新过期的token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import store from '@/store/'
 import JSONBig from 'json-bigint'
+import { Toast } from 'vant'
+import router from '@/router/'
 
 const request = axios.create({
   // baseURL: 'http://ttapi.research.itcast.cn/app/'
@@ -16,6 +18,10 @@ const request = axios.create({
   ]
 })
 
+const refreshTokenReq = axios.create({
+  baseURL: '/api'
+})
+
 // 请求拦截器
 request.interceptors.request.use(
   config => {
@@ -26,6 +32,53 @@ request.interceptors.request.use(
     return config
   },
   error => {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+request.interceptors.response.use(
+  response => {
+    // 响应成功进入这里
+    return response
+  },
+  async error => {
+    // 响应失败进入这里
+    const status = error.response.status
+    if (status === 400) {
+      Toast.fail('请求参数错误')
+    } else if (status === 401) {
+      // 1. 如果没有 token，直接跳转到登录页
+      const userToken = store.state.UserMod.token
+      if (!userToken || !userToken.token) {
+        return router.replace('/login')
+      }
+      // 2. 如果有 token，尝试用 refresh_token 发请求
+      try {
+        const res = await refreshTokenReq({
+          method: 'PUT',
+          url: '/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${userToken.refresh_token}`
+          }
+        })
+        // 如果请求成功，刷新 token
+        userToken.token = res.data.data.token
+        // （注意模块内 mutations 的调用方法）
+        store.commit('UserMod/setToken', userToken)
+        // 继续发送原来的请求
+        return request(error.config)
+      } catch (ex) {
+        // 如果请求失败，直接跳转到登录页
+        return router.replace('/login')
+      }
+    } else if (status === 403) {
+      Toast.fail('操作没有权限')
+    } else if (status >= 500) {
+      Toast.fail('服务器异常，请稍后重试')
+    }
+
+    // 抛出异常
     return Promise.reject(error)
   }
 )
