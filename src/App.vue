a13ab1e845dbd4e800ff5ee1a5adcb2426c9977e
Shallowbeing <load_more@163.com>
2021-08-18 18:45:31 +0800

    feat: 条件缓存 - 限制缓存的作用范围，解决文章详情被缓存无法更新的问题（只让Layout组件被缓存）

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <!-- 路由出口 -->
     <!-- 一级路由的组件缓存 -->
-    <keep-alive>
+    <!-- 条件缓存：一级路由中只有 LayoutIndex 组件被缓存 -->
+    <keep-alive :include="['LayoutIndex']">
       <router-view />
     </keep-alive>
   </div>
